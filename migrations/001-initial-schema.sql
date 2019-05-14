67f8295a169dfc279b8a2b579c60526311771ced
Devin Collins <me@imdevinc.com>
2019-05-14 16:14:28 -0600

    Adds username migration for Ombi permissions

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,7 +1,7 @@
 -- Up
 CREATE TABLE general (id integer primary key asc, username text, password text);
 CREATE TABLE bot (id integer primary key asc, token text, ownerid text, commandprefix text, deletecommandmessages text, unknowncommandresponse text);
-CREATE TABLE ombi (id integer primary key asc, host text, port text, apikey text, requesttv text, requestmovie text, username text);
+CREATE TABLE ombi (id integer primary key asc, host text, port text, apikey text, requesttv text, requestmovie text);
 CREATE TABLE tautulli (id integer primary key asc, host text, port text, apikey text);
 CREATE TABLE sonarr (id integer primary key asc, host text, port text, apikey text);
 CREATE TABLE radarr (id integer primary key asc, host text, port text, apikey text);
