6bef754f2f78902d6d0d418925dda8b8db060dde
void <voidp@protonmail.com>
2020-01-23 12:13:33 +0100

    added Mellow version into startup logs

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ const BotClient = require('./BotClient.js');
 const WebServer = require('./WebServer.js');
 const Database = require('./Database.js');
 const { migrateALL } = require('./migration/migration.js');
+const { version } = require('../package.json');
 
-const start = function () {
-    migrateALL().then(() => {
-        let bot;
-        const webDatabase = new Database();
-        const botConfig = webDatabase.webConfig.bot;
-        if (botConfig && botConfig.token) {
-            bot = new BotClient(webDatabase, botConfig.ownerid, botConfig.commandprefix);
-            bot.init().catch((err) => {
-                console.log('Failed initializing DiscordBot! Please check your bot configurations.');
-                console.error(err);
-            });
-        } else console.log('There is no bot token provided. Please check your configurations.');
-        new WebServer(webDatabase, bot).init();
-    }).catch(console.error);
-}
-
-start();
+console.log(`Mellow v${version}`);
+migrateALL().then(() => {
+    let bot;
+    const webDatabase = new Database();
+    const botConfig = webDatabase.webConfig.bot;
+    if (botConfig && botConfig.token) {
+        bot = new BotClient(webDatabase, botConfig.ownerid, botConfig.commandprefix);
+        bot.init().catch((err) => {
+            console.log('Failed initializing DiscordBot! Please check your bot configurations.');
+            console.error(err);
+        });
+    } else console.log('There is no bot token provided. Please check your configurations.');
+    new WebServer(webDatabase, bot).init();
+}).catch(console.error);
