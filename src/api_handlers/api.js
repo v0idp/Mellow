a4dd42496b853907de187021bae2b6b35fe8ef48
void <voidp@protonmail.com>
2020-01-30 00:45:42 +0100

    added tvmaze api to check if tv show is anime or not

--- src/api_handlers/api.js
+++ src/api_handlers/api.js
@@ -2,6 +2,7 @@ const Ombi = require('./ombi.js');
 const Radarr = require('./radarr.js');
 const Sonarr = require('./sonarr.js');
 const Tautulli = require('./tautulli.js');
+const TVMaze = require('./tvmaze.js');
 
 class APIHandler {
     constructor (config) {
@@ -9,6 +10,7 @@ class APIHandler {
         this.radarr = new Radarr(config.radarr);
         this.sonarr = new Sonarr(config.sonarr);
         this.tautulli = new Tautulli(config.tautulli);
+        this.tvmaze = new TVMaze();
     }
 }
 
