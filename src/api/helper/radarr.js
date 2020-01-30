fdb16a7dd3d28aa082a97b544db99a3476775ff8
void <voidp@protonmail.com>
2020-01-30 21:02:51 +0100

    added helper function for radarr to build Movie object

--- src/api/helper/radarr.js
+++ src/api/helper/radarr.js
@@ -1,1 +1,27 @@
-// TODO: Add helper for Radarr Movie
+module.exports = buildRadarrMovie = (movie, radarr, searchNow = false) => {
+    if (radarr.profile === "0" || radarr.rootfolder === "0") {
+        console.log("Please set quality profile and default root folder in radarr config!");
+        return undefined;
+    }
+
+    let newMovie = {
+        title = movie.title,
+        tmdbId = movie.tmdbId,
+        qualityProfileId = parseInt(radarr.profile),
+        rootFolderPath = parseInt(radarr.rootfolder),
+        titleSlug = movie.titleSlug,
+        monitored = true,
+        year = movie.year,
+        images = movie.images,
+        minimumAvailability = radarr.minimumavailability
+    };
+
+    newMovie = {
+        ...newMovie,
+        addOptions: {
+            searchForMovie: searchNow
+        }
+    }
+
+    return newMovie;
+}
