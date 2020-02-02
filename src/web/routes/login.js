37732490c82a93dbd7c5a2f3ecff906f77b1028f
void <voidp@protonmail.com>
2020-02-02 01:14:06 +0100

    login page will redirect to /config now if token is valid in cookie

--- src/web/routes/login.js
+++ src/web/routes/login.js
@@ -1,8 +1,12 @@
 const login = require('../controllers/login.js');
 const router = require('express-promise-router')();
+const passport = require('passport');
+const passStrats = require('../config/passport.js');
+const passJwt = passport.authenticate('userJwt', { successRedirect: '/config' });
 
 router.route('/').get(login.render);
 router.route('/').post(login.login);
 router.route('/logout').get(login.logout);
+router.route('/verify').get(passJwt);
 
 module.exports = router;
